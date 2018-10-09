602dc2078007ec8b7d249fc22d1bb65046112b08
MoondanceZ <QYK_zZ@hotmail.com>
2018-10-09 20:46:31 +0800

    update: query

--- src/main/java/com/rk/controller/BaseController.java
+++ src/main/java/com/rk/controller/BaseController.java
@@ -1,11 +1,13 @@
 package com.rk.controller;
 
 import org.apache.commons.text.StringEscapeUtils;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
 import java.beans.PropertyEditorSupport;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by Qin_Yikai on 2018-10-09.
@@ -18,7 +20,7 @@ public class BaseController {
         binder.registerCustomEditor(String.class, new PropertyEditorSupport() {
             @Override
             public void setAsText(String text) {
-                setValue(text == null ? null : StringEscapeUtils.escapeHtml4(text));
+                setValue(text == null ? null : StringEscapeUtils.escapeHtml4(text.trim()));
             }
 
             @Override
@@ -27,5 +29,9 @@ public class BaseController {
                 return value != null ? value.toString() : "";
             }
         });
+
+        //日期类型转换
+        /*binder.registerCustomEditor(String.class,
+                new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), true));*/
     }
 }
