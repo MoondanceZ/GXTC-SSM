e67a2e171bd5d7b99848e94fecc7b074758de751
MoondanceZ <QYK_zZ@hotmail.com>
2018-10-13 18:55:47 +0800

    update: request case

---
+++ src/main/java/com/rk/common/config/SpringWebConfig.java
@@ -0,0 +1,17 @@
+package com.rk.common.config;
+
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * Created by Qin_Yikai on 2018-10-13.
+ */
+public class SpringWebConfig  extends WebMvcConfigurationSupport {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        AntPathMatcher pathMatcher = new AntPathMatcher();
+        pathMatcher.setCaseSensitive(false);
+        configurer.setPathMatcher(pathMatcher);
+    }
+}
---
+++ src/main/java/com/rk/common/filter/CaseInsensitiveRequestParameterNameFilter.java
@@ -0,0 +1,63 @@
+package com.rk.common.filter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.LinkedCaseInsensitiveMap;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+
+/**
+ * Created by Qin_Yikai on 2018-10-13.
+ */
+@Configuration
+public class CaseInsensitiveRequestParameterNameFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(new CaseInsensitiveParameterNameHttpServletRequest(httpServletRequest), httpServletResponse);
+    }
+
+    public static class CaseInsensitiveParameterNameHttpServletRequest extends HttpServletRequestWrapper {
+        private final LinkedCaseInsensitiveMap<String[]> map = new LinkedCaseInsensitiveMap<>();
+
+        @SuppressWarnings("unchecked")
+        public CaseInsensitiveParameterNameHttpServletRequest(HttpServletRequest request) {
+            super(request);
+            map.putAll(request.getParameterMap());
+        }
+
+        @Override
+        public String getParameter(String name) {
+
+            String[] array = this.map.get(name);
+            if (array != null && array.length > 0)
+                return array[0];
+            return null;
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap() {
+            return Collections.unmodifiableMap(this.map);
+        }
+
+        @Override
+        public Enumeration<String> getParameterNames() {
+            return Collections.enumeration(this.map.keySet());
+        }
+
+        @Override
+        public String[] getParameterValues(String name) {
+            return this.map.get(name);
+        }
+
+    }
+
+}
